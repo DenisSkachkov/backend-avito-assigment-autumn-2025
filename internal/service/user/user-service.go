8128a1fbc545fd104bfb3c776143eae25f83e409
DenisSkachkov <Denprymeevans@gmail.com>
2025-11-14 16:02:17 +0300

    Created User service

---
+++ internal/service/user/user-service.go
@@ -0,0 +1,21 @@
+package user
+
+import (
+	"context"
+
+	"github.com/DenisSkachkov/backend-avito-assigment-autumn-2025/internal/models"
+	"github.com/DenisSkachkov/backend-avito-assigment-autumn-2025/internal/service"
+)
+
+type UserService struct {
+	repo UserRepository
+}
+
+func (s *UserService) SetActive(ctx context.Context, id string, active bool) (*models.User, error) {
+	user, err := s.repo.GetUserById(ctx, id)
+	if err != nil {
+		return nil, service.ErrUserNotFound
+	}
+	user.IsActive = active
+	return user, s.repo.Update(ctx, user)
+}
