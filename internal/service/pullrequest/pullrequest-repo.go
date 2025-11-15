a3601ee57945245925ca138531f8d6781504f6c7
DenisSkachkov <Denprymeevans@gmail.com>
2025-11-15 16:11:35 +0300

    Added Update func to PR interface

--- internal/service/pullrequest/pullrequest-repo.go
+++ internal/service/pullrequest/pullrequest-repo.go
@@ -8,6 +8,8 @@ import (
 
 type PullRequestRepository interface {
 	GetPullRequestsByReviewerId(ctx context.Context, reviewerId string) ([]*models.PullRequest, error)
+	GetByID(ctx context.Context, id string) (*models.PullRequest, error)
 	ExistsByName(ctx context.Context, authorId, name string) (bool, error)
 	Create(ctx context.Context,pr *models.PullRequest) error
+	Update(ctx context.Context, pr *models.PullRequest) error
 }
