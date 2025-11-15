2d291caa441624fb68047cdc83a3273ebb446590
DenisSkachkov <Denprymeevans@gmail.com>
2025-11-15 16:00:39 +0300

    Added field MergedAt into PullRequest struct

--- internal/models/pullrequest.go
+++ internal/models/pullrequest.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type PullRequest struct {
-	PullRequestId string `json:"pull_request_id"`
-	PullRequestName string `json:"pull_request_name"`
-	AuthorId string `json:"author_id"`
-	Status string `json:"status"`
+	PullRequestId     string   `json:"pull_request_id"`
+	PullRequestName   string   `json:"pull_request_name"`
+	AuthorId          string   `json:"author_id"`
+	Status            string   `json:"status"`
 	AssignedReviewers []string `json:"assigned_reviewers"`
+	MergedAt          time.Time
 }
