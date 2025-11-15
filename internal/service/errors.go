2642209b62fe2de6f7e38d05a282746a50cd105a
DenisSkachkov <Denprymeevans@gmail.com>
2025-11-15 16:29:20 +0300

    Added ErrNotAssigned and ErrNoCandidate errors

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -10,3 +10,7 @@ var ErrNotFound = errors.New("NOT_FOUND")
 
 var ErrPullRequestMerged = errors.New("PR_MERGED")
 
+var ErrNotAssigned = errors.New("NOT_ASSIGNED")
+
+var ErrNoCandidate = errors.New("NO_CANDIDATE")
+
