747bc68486b750da0cd58437793a6d03f9b9b5a7
DenisSkachkov <Denprymeevans@gmail.com>
2025-11-16 20:29:27 +0300

    Added Migrations

---
+++ migrations/0001_init.down.sql
@@ -0,0 +1,15 @@
+DROP INDEX IF EXISTS idx_pr_reviewers_reviewer;
+
+DROP INDEX IF EXISTS idx_team_members_user;
+
+DROP INDEX IF EXISTS ux_pr_author_name;
+
+DROP TABLE IF EXISTS pr_reviewers;
+
+DROP TABLE IF EXISTS pull_requests;
+
+DROP TABLE IF EXISTS team_members;
+
+DROP TABLE IF EXISTS teams;
+
+DROP TABLE IF EXISTS users;
